e331da1516fd64d846a03c74806dd5cb492d1861
rpupunu <rpupunu@gmail.com>
2019-11-22 21:47:53 -0700

    removed paritials

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -28,7 +28,7 @@ $(function() {
   
       var newBurger = {
         burger_name: $("#bu").val().trim(),
-        devoured: $("[name=devoured]:checked").val().trim()
+        devoured: 0
       };
   
       // Send the POST request.
